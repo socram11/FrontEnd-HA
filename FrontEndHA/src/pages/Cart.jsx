d4e1b273f9f682569515011adeb24b6de90f2262
Pablo-m22 <mpablo2042@gmail.com>
2025-06-12 19:53:32 -0300

    Cart

--- FrontEndHA/src/pages/Cart.jsx
+++ FrontEndHA/src/pages/Cart.jsx
@@ -8,8 +8,8 @@ const Cart = () => {
         <div className="d-flex pb-4 ">
           <h3>Carrito</h3>
         </div>
-        <div className="d-flex row justify-content-center">
-          <div className="d-flex flex-column justify-content-between col-8">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-8 d-flex flex-column gap-3">
             <div className="row p-3 g-2 border border-black">
               <div className="col-4 px-5">product.img and data</div>
               <div className="col-4 px-5">change quantity</div>
@@ -31,17 +31,17 @@ const Cart = () => {
               <div className="col-4 px-5">Pricce</div>
             </div>
           </div>
-          <div className="col-4 d-flex flex-column justify-content-between py-3">
+          <div className="col-12 col-md-4 d-flex flex-column justify-content-between py-3 mt-4 mb-5 mb-md-0 gap-3">
             <div>
               <textarea
-                className="form-control"
+                className="form-control mb-3"
                 placeholder="Notas adicionales"
               ></textarea>
             </div>
-            <div>$TotalPrice</div>
-            <div>
+            <div className="mb-3 text-center">$TotalPrice</div>
+            <div className="text-center">
               <Link to={"/checkout"}>
-                <button>Go to checkOut</button>
+                <button className="btn btn-dark fw-bold">Go to checkOut</button>
               </Link>
             </div>
           </div>
